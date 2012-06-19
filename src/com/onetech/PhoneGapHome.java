70870d08e56cb225422e0d06b8e03b250e66f24d
ravi <ravi@ravi-PC>
2012-06-19 18:19:39 +0530

    First Commit

---
+++ src/com/onetech/PhoneGapHome.java
@@ -0,0 +1,14 @@
+package com.onetech;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+
+public class PhoneGapHome extends DroidGap {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       super.loadUrl("file:///android_asset/www/index.html"); 
+    }
+}
